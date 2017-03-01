fad3c57511ce40aa606ebcfa398534f6cd534a05
jboundy <j.a.boundy@gmail.com>
2017-03-01 02:46:52 -0600

    Added more sound files
    Added documentation that outputs to text channel !breadme

--- BoundyBotNet/Program.cs
+++ BoundyBotNet/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using BoundyBotNet.helpers;
 using Discord;
 using Discord.Audio;
-using NAudio.FileFormats.Mp3;
-using NAudio.Wave;
 
 namespace BoundyBotNet
 {
@@ -13,20 +11,19 @@ namespace BoundyBotNet
     {
         static void Main(string[] args)
         {
-            //var host = new JobHost();
-            //// The following code ensures that the WebJob will be running continuously
-            //host.Call(typeof(Program).GetMethod("Start"));
-            //host.RunAndBlock();
-
-            new Program().Start();
-           
+            new Program().Start();        
         } 
-
-        public DiscordClient _client;
 
         public void Start()
         {
-            _client = new DiscordClient();
+            var botHelper = new BotHelpers();
+            var builder = new StringBuilder();
+            foreach (var item in botHelper.CommandList())
+            {
+                builder.Append(item).Append(Environment.NewLine);
+            }
+            var commandList = builder.ToString();
+            var _client = botHelper.Client;
 
             _client.UsingAudio(x => 
             {
@@ -36,130 +33,146 @@ namespace BoundyBotNet
             _client.MessageReceived += async (s, e) =>
             {
                 DirectoryInfo dir = new DirectoryInfo("sounds");
+
+                if (e.Message.Text.Equals("!breadme"))
+                {
+                    await e.Message.Channel.SendMessage(commandList);               
+                }
+
                 if (e.Message.Text.Equals("!bhorn"))
                 {            
-                    await PlayAudioAsync($@"{dir.FullName}\airhorn_default.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\airhorn_default.wav");
                 }
 
                 if (e.Message.Text.Equals("!bfuck"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\fuckoff.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\fuckoff.wav");
                 }
 
                 if (e.Message.Text.Equals("!bhugeb"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\huge-bitch.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\huge-bitch.wav");
                 }
 
                 if (e.Message.Text.Equals("!bff7"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\victory_fanfare.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\victory_fanfare.wav");
                 }
 
                 if (e.Message.Text.Equals("!bcombo"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\combobreaker.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\combobreaker.wav");
                 }
 
                 if (e.Message.Text.Equals("!bprincess"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\Excuse_Me_Princess.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\Excuse_Me_Princess.wav");
                 }
 
                 if (e.Message.Text.Equals("!bhotstep"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\hotstepper.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\hotstepper.wav");
                 }
 
                 if (e.Message.Text.Equals("!bchest"))
                 {                   
-                   await PlayAudioAsync($@"{dir.FullName}\zeldaitem.wav");               
+                   await botHelper.PlayAudioAsync($@"{dir.FullName}\zeldaitem.wav");               
                 }
 
                 if (e.Message.Text.Equals("!bburn"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\burned.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\burned.wav");
                 }
 
                 if (e.Message.Text.Equals("!bdrama"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\drama.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\drama.wav");
                 }
 
                 if (e.Message.Text.Equals("!bfatal"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\fatality.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\fatality.wav");
                 }
 
                 if (e.Message.Text.Equals("!bfdp"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\fdp.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\fdp.wav");
                 }
 
                 if (e.Message.Text.Equals("!bmgs"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\metalgearsolid.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\metalgearsolid.wav");
                 }
 
                 if (e.Message.Text.Equals("!bsparta"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\thisissparta.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\thisissparta.wav");
                 }
 
                 if (e.Message.Text.Equals("!bwolo"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\wololo.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\wololo.wav");
                 }
 
                 if (e.Message.Text.Equals("!bsecret"))
                 {
-                    await PlayAudioAsync($@"{dir.FullName}\zeldasecret.wav");
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\zeldasecret.wav");
+                }
+
+                if (e.Message.Text.Equals("!bpardon"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\beg-your-pardon-sir.wav");
+                }
+
+                if (e.Message.Text.Equals("!bwhip"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\crack_the_whip.wav");
+                }
+
+                if (e.Message.Text.Equals("!bnuts"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\deez-nuts.wav");
+                }
+
+                if (e.Message.Text.Equals("!bnope"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\nope.wav");
+                }
+
+                if (e.Message.Text.Equals("!bomg"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\omg-who-the-hell-cares_2.wav");
+                }
+
+                if (e.Message.Text.Equals("!bobjection"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\phoenix-objection.wav");
+                }
+
+                if (e.Message.Text.Equals("!bstfu"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\shut-the-fuck-up.wav");
+                }
+
+                if (e.Message.Text.Equals("!bsurprise"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\surprise-motherfucker.wav");
+                }
+
+                if (e.Message.Text.Equals("!btada"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\tada.wav");
+                }
+
+                if (e.Message.Text.Equals("!bholyfuck"))
+                {
+                    await botHelper.PlayAudioAsync($@"{dir.FullName}\yang-holy-fuck.wav");
                 }
             };
 
            _client.ExecuteAndWait(async () => {
                 await _client.Connect("MjQ5NzkzMjMyNTM4NDM1NTg1.CxLdsg.QzojNKXFJOnPwez_ByUEkKEg4I8", TokenType.Bot);
             });
-        }
-
-        private async Task<IAudioClient> JoinVoiceChannelAsync(ulong channelId)
-        {
-            var voiceChannel = _client.FindServers("Jon's Cave of Fun").FirstOrDefault()
-                .VoiceChannels.FirstOrDefault(x => x.Id == channelId);
-            return await _client.GetService<AudioService>().Join(voiceChannel);
-        }
-
-        private async Task PlayAudioAsync(string filePath)
-        {
-            var _vClient = await JoinVoiceChannelAsync(202186271999787009);
-            var channelCount = _client.GetService<AudioService>().Config.Channels;
-            var outFormat = new WaveFormat(48000, 16, 2);
-            var length = Convert.ToInt32(outFormat.AverageBytesPerSecond / 60.0 * 1000.0);
-            byte[] buffer = new byte[length];
-
-            using (var reader = new WaveFileReader(filePath))
-            {
-                using (var resampler = new WaveFormatConversionStream(outFormat, reader))
-                {
-                    int count = 0;
-                    while ((count = resampler.Read(buffer, 0, length)) > 0)
-                    {
-                        _vClient.Send(buffer, 0, count);
-                    }
-                }
-            }
-
-            //todo: maybe someday we can use mp3s
-            //using (var reader = new Mp3FileReader(filePath))
-            //{
-            //    using (var resampler = new DmoMp3FrameDecompressor(outFormat))
-            //    {
-            //        int count = 0;
-            //        while((count = resampler.))
-            //    }
-            //}
-            _vClient.Wait();
-            await _vClient.Disconnect();
         }
     }
 }
---
+++ BoundyBotNet/helpers/BotHelpers.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using NAudio.Wave;
+
+namespace BoundyBotNet.helpers
+{
+    public class BotHelpers
+    {
+        public DiscordClient Client;
+
+        public BotHelpers()
+        {
+            Client = new DiscordClient();
+        }
+
+        private async Task<IAudioClient> JoinVoiceChannelAsync(ulong channelId)
+        {
+            var voiceChannel = Client.FindServers("Jon's Cave of Fun").FirstOrDefault()
+                .VoiceChannels.FirstOrDefault(x => x.Id == channelId);
+            return await Client.GetService<AudioService>().Join(voiceChannel);
+        }
+
+        public async Task PlayAudioAsync(string filePath)
+        {
+            var _vClient = await JoinVoiceChannelAsync(202186271999787009);
+            var channelCount = Client.GetService<AudioService>().Config.Channels;
+            var outFormat = new WaveFormat(48000, 16, 2);
+            var length = Convert.ToInt32(outFormat.AverageBytesPerSecond / 60.0 * 1000.0);
+            byte[] buffer = new byte[length];
+
+            using (var reader = new WaveFileReader(filePath))
+            {
+                using (var resampler = new WaveFormatConversionStream(outFormat, reader))
+                {
+                    int count = 0;
+                    while ((count = resampler.Read(buffer, 0, length)) > 0)
+                    {
+                        _vClient.Send(buffer, 0, count);
+                    }
+                }
+            }
+
+            //todo: maybe someday we can use mp3s
+            //using (var reader = new Mp3FileReader(filePath))
+            //{
+            //    using (var resampler = new DmoMp3FrameDecompressor(outFormat))
+            //    {
+            //        int count = 0;
+            //        while((count = resampler.))
+            //    }
+            //}
+            _vClient.Wait();
+            await _vClient.Disconnect();
+        }
+
+        public IEnumerable<string> CommandList() =>
+            new List<string>
+            {
+                "!bhorn - Mighty horn",
+                "!bfuck - Angry",
+                "!bhugeb - Big Huge Bitch",
+                "!bff7 -Final Fantasy victory",
+                "!bcombo - CCCCombo breaker",
+                "!bprincess - Well princess....",
+                "!bhotstep - Alex might like this",
+                "!bchest - Zelda chest opening",
+                "!bburn - Git burnt",
+                "!bdrama - Dramatic beaver",
+                "!bfatal - MK Fatality",
+                "!bfdp - Some funny spanish",
+                "!bmgs - Metal Gear Solid alert",
+                "!bsparta - This...is...SPARTA!!!",
+                "!bwolo - Wololololollo",
+                "!bsecret - Zelda Secret",
+                "!bpardon - Pardon me...",
+                "!bwhip - Its a whip",
+                "!bnuts - Deez nuts",
+                "!bnope - Nope.avi",
+                "!bomg - Family guy reference",
+                "!bobjection - Objection!",
+                "!bstfu - Just stfu",
+                "!bsurprise - Surprise mothafucker",
+                "!btada - Tada.....",
+                "!bholyfuck - Really makes me giggle"
+            };  
+    }
+}
